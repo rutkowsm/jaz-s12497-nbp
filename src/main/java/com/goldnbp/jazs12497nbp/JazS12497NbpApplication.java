911eebbe4684f362deab93d32a45f24ffd9db169
s12497@pjwstk.edu.pl <Angelo0000>
2022-06-18 11:57:52 +0200

    Initial commit

---
+++ src/main/java/com/goldnbp/jazs12497nbp/JazS12497NbpApplication.java
@@ -0,0 +1,13 @@
+package com.goldnbp.jazs12497nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS12497NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS12497NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldnbp/jazs12497nbp/JazS12497NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldnbp.jazs12497nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS12497NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
